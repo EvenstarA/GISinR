282b2359f27e99dbe9798d453ebb5dcf85edd3d2
EvenstarA <kmmarw001@myuct.ac.za>
2025-02-26 10:07:20 +0200

    cleaning

--- GIS.R
+++
@@ -1,9 +0,0 @@
-# Title: GIS in R Project - Honours, February 2025
-# Author: Arwenn Kummer
-# Date Created: 24/02/2025  
-
-
-# Install relevant packages for GIS 
-
-install.packages(c("tidyverse", "sf", "lwgeom", "terra", "stars", "exactextractr"))
-install.packages(c("cowplot", "hrbrthemes", "knitr", "leaflet", "htmltools", "rosm", "ggspatial", "rnaturalearth", "mapview", "tmap"))
